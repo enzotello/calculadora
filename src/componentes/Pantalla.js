cee30c1665e9c69df17702dc9a3fb31f37960795
enzotello <enzotello0@gmail.com>
2023-05-23 18:29:12 -0300

    Primer comit

---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../hojas-de-estilo/Pantalla.css";
+
+const Pamtalla = ({ input }) => (
+  <div className="input">
+    {input}
+  </div>
+);
+export default Pamtalla;
